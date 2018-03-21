4c6f6f16ab1ff460e222e71cd4db6bcd34180db6
Missy Beutler <missyjrussell@gmail.com>
2018-03-21 12:55:49 -0600

    updated backend code

--- server/controllers/books_controller.js
+++ server/controllers/books_controller.js
@@ -2,34 +2,38 @@ let books = [];
 let id = 0;
 
 module.exports = {
-  create: ( req, res ) => {
-    const { title, author } = req.body;
-    books.push( { id, title, author } );
-    id++;
-    res.status(200).send( books );
-  },
-
   read: ( req, res ) => {
     res.status(200).send( books );
   },
-
+  create: ( req, res ) => {
+    const { title, author } = req.body;
+    let book = {
+      id: id,
+      title: title,
+      author: author
+    }
+    books.push( book );
+    id++;
+    res.status(200).send( books );
+  },
   update: ( req, res ) => {
-    const updateID = req.params.id;
-    let index = books.findIndex( book => book.id == updateID );
-
+    let index = null;
+    books.forEach((book, i) => {
+      if(book.id === Number(req.params.id)) index = i;
+    })
     books[ index ] = {
       id: books[ index ].id,
       title: req.body.title || books[ index ].title,
       author: req.body.author || books[ index ].author
     };
-
     res.status(200).send( books );
   },
-
   delete: ( req, res ) => {
-    const deleteID = req.params.id;
-    bookID = books.findIndex( book => book.id == deleteID );
-    books.splice( bookID, 1 );
+    let index = null;
+    books.forEach((book, i) => {
+      if(book.id === Number(req.params.id)) index = i;
+    })
+    books.splice(index, 1)
     res.status(200).send( books );
   }
 };
--- server/index.js
+++ server/index.js
@@ -1,17 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const bc = require( __dirname + '/controllers/books_controller');
+const bc = require('./controllers/books_controller.js');
 
 const app = express();
 
 app.use( bodyParser.json() );
-app.use( express.static( __dirname + "/../public/build") );
+app.use( express.static( __dirname + "/../build") );
 
-const baseURL = "/api/books";
-app.post(baseURL, bc.create);
-app.get(baseURL, bc.read);
-app.put(`${baseURL}/:id`, bc.update);
-app.delete(`${baseURL}/:id`, bc.delete);
+app.get('/api/books', bc.read);
+app.post('/api/books', bc.create);
+app.put('/api/books/:id', bc.update);
+app.delete('/api/books/:id', bc.delete)
 
 const port = 3000;
 app.listen( port, () => { console.log(`Server listening on port ${port}`); } );
