9f0aa9fad958b315ef5a0f1113ad87d9d7cc107e
Beth Telford <mrsbethtelford@gmail.com>
2019-03-21 02:08:45 -0600

    replace body-parser

--- server/controllers/books_controller.js
+++ server/controllers/books_controller.js
@@ -2,38 +2,38 @@ let books = [];
 let id = 0;
 
 module.exports = {
-  read: ( req, res ) => {
-    res.status(200).send( books );
+  read: (req, res) => {
+    res.status(200).send(books);
   },
-  create: ( req, res ) => {
+  create: (req, res) => {
     const { title, author } = req.body;
     let book = {
       id: id,
       title: title,
       author: author
-    }
-    books.push( book );
-    id++;
-    res.status(200).send( books );
-  },
-  update: ( req, res ) => {
-    let index = null;
-    books.forEach((book, i) => {
-      if(book.id === Number(req.params.id)) index = i;
-    })
-    books[ index ] = {
-      id: books[ index ].id,
-      title: req.body.title || books[ index ].title,
-      author: req.body.author || books[ index ].author
     };
-    res.status(200).send( books );
+    books.push(book);
+    id++;
+    res.status(200).send(books);
   },
-  delete: ( req, res ) => {
+  update: (req, res) => {
     let index = null;
     books.forEach((book, i) => {
-      if(book.id === Number(req.params.id)) index = i;
-    })
-    books.splice(index, 1)
-    res.status(200).send( books );
+      if (book.id === Number(req.params.id)) index = i;
+    });
+    books[index] = {
+      id: books[index].id,
+      title: req.body.title || books[index].title,
+      author: req.body.author || books[index].author
+    };
+    res.status(200).send(books);
+  },
+  delete: (req, res) => {
+    let index = null;
+    books.forEach((book, i) => {
+      if (book.id === Number(req.params.id)) index = i;
+    });
+    books.splice(index, 1);
+    res.status(200).send(books);
   }
 };
